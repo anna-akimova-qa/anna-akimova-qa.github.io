55eec90882fbc6b9d3f58903c952affc9680dbd6
Анна <anna.akimova.qa@gmail.com>
2024-11-09 12:05:41 +0300

    Update js.js

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	 <img src="https://otvet.imgsmail.ru/download/5204691_1d56f1096c4d39f3b1c5fcd5a237f3a1_800.gif" />
-})
+	 alert('Главное - верить в себя! Мнение окружающих меняется ежедневно.')
+	})
