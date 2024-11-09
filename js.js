eeb01f59cad1580e7adcafe87aa5376e60377ec6
anna-akimova-qa <anna.akimova.qa@gmail.com>
2024-11-09 11:13:25 +0300

    Файлы сайта-визитки

---
+++ js.js
@@ -0,0 +1,7 @@
+/* Это объявление переменной, мы наши кнопку по тегу */
+const button = document.querySelector('button');
+
+/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+button.addEventListener('click', function() {
+	 <img src="https://otvet.imgsmail.ru/download/5204691_1d56f1096c4d39f3b1c5fcd5a237f3a1_800.gif" />
+})
